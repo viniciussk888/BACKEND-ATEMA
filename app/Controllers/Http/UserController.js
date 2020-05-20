4cc03cf0c33acd47c1335ffdc29a2a342d6c25d6
viniciussk888 <vinicius.cross07@gmail.com>
2020-05-20 17:36:51 -0300

    'users'

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,8 +3,13 @@
 const User = use('App/Models/User')
 
 class UserController {
+  async index({ }) {
+    const user = await User.all();
+
+    return user
+  }
   async store({ request }) {
-    const data = request.only(['username', 'password', 'email'])
+    const data = request.only(['username', 'password', 'email', 'delete', 'insert', 'update', 'blog', 'admin'])
 
     const user = await User.create(data)
 
--- start/routes.js
+++ start/routes.js
@@ -3,13 +3,15 @@
 
 const Route = use('Route')
 
-Route.post('users', 'UserController.store');
+
 Route.post('sessions', 'SessionController.store');
 //recuperar senha
-Route.post('passwords', 'ForgotPasswordController.store')
+//Route.post('passwords', 'ForgotPasswordController.store')
 //ATEMA
 
 Route.group(() => {
+  Route.post('users', 'UserController.store');
+  Route.get('users', 'UserController.index');
   Route.resource('atema', 'AtemaController').apiOnly()
   Route.resource('post', 'PostController').apiOnly()
 }).middleware(['auth'])
